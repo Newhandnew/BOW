e703ed428ceaee9977e176d5053971b7a2c17583
Newhandnew <easytired@gmail.com>
2017-04-27 11:34:31 +0800

    collect images and do SURF feature

---
+++ CMakeFiles/bow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bow.dir/bow.cpp.o"
+  "bow.pdb"
+  "bow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bow.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace cv::xfeatures2d;
+int numBOF = 10;
+
+/* @function main */
+int main( int argc, char** argv )
+{
+	VideoCapture cap(0);
+	if(cap.isOpened() != 1)
+	{
+		printf("error in capturincd g");
+		return -1;
+	}
+	
+	namedWindow("video", 1);
+	Mat cameraFrame, imgGray;
+	//-- Step 1: Detect the keypoints using SURF Detector
+  	int minHessian = 400;
+  	Mat imgKeypoints;
+
+  	Ptr<SURF> detector = SURF::create( minHessian );
+  	std::vector<KeyPoint> keypoints;
+  	//To store the SURF descriptor of current image
+  	Mat descriptor;
+	//To store all the descriptors that are extracted from all the images.
+	Mat featuresUnclustered;
+
+
+	for (int i = 0; i < numBOF; i++)
+	{
+		clock_t start = clock();
+		cap >> cameraFrame;
+		cvtColor(cameraFrame, imgGray, COLOR_BGR2GRAY);
+		detector->detectAndCompute( imgGray, Mat(), keypoints, descriptor );
+		featuresUnclustered.push_back(descriptor);
+		clock_t end = clock();
+		float spendSeconds = (float)(end - start) / CLOCKS_PER_SEC;
+
+		drawKeypoints( cameraFrame, keypoints, imgKeypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+		imshow("video", imgKeypoints);
+
+		printf("%i percent done, time: %f \n", (i + 1) * 100 / numBOF, spendSeconds);
+		if(waitKey(30) == 27)
+		{
+			printf("exit");
+			break;
+		}
+	}
+	return 0;
+}
