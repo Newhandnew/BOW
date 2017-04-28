4afb63ca51202a31d3c151f2081b18ca970be559
Newhandnew <easytired@gmail.com>
2017-04-28 17:43:59 +0800

    store all the training data as keyframe for classification, need to refactor

--- bow.cpp
+++ bow.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <time.h>
+#include <sstream>
 #include "opencv2/core.hpp"
 #include "opencv2/features2d.hpp"
 #include "opencv2/xfeatures2d.hpp"
@@ -9,7 +10,11 @@
 
 using namespace cv;
 using namespace cv::xfeatures2d;
+using namespace std;
+
 int numBOF = 20;
+string imgDir = "imgEnv/";
+string imgType = ".jpg";
 
 /* @function main */
 int main( int argc, char** argv )
@@ -28,12 +33,14 @@ int main( int argc, char** argv )
   	Mat imgKeypoints;
 
   	Ptr<SURF> detector = SURF::create( minHessian );
-  	std::vector<KeyPoint> keypoints;
+  	vector<KeyPoint> keypoints;
   	//To store the SURF descriptor of current image
   	Mat descriptor;
 	//To store all the descriptors that are extracted from all the images.
 	Mat featuresUnclustered;
+	stringstream imgName;
 
+	
 	for (int i = 0; i < numBOF; i++)
 	{
 		clock_t start = clock();
@@ -46,6 +53,9 @@ int main( int argc, char** argv )
 
 		drawKeypoints( cameraFrame, keypoints, imgKeypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
 		imshow("video", imgKeypoints);
+
+		imgName << imgDir << i << imgType;
+		imwrite(imgName.str(), cameraFrame);
 
 		printf("%i percent done, time: %f \n", (i + 1) * 100 / numBOF, spendSeconds);
 		if(waitKey(30) == 27)
@@ -64,6 +74,9 @@ int main( int argc, char** argv )
 	FileStorage fs("dictionary.yml", FileStorage::WRITE);
 	fs << "vocabulary" << dictionary;
 	fs.release();
+	FileStorage fTrainFeature("trainImgFeature.yml", FileStorage::WRITE);
+	fTrainFeature << "train image feature" << featuresUnclustered;
+	fTrainFeature.release();
 
 	return 0;
 }
--- compareBOF.cpp
+++ compareBOF.cpp
@@ -9,6 +9,11 @@
 
 using namespace cv;
 using namespace cv::xfeatures2d;
+using namespace std;
+
+int numBOF = 20;
+string imgDir = "imgEnv/";
+string imgType = ".jpg";
 
 int main( int argc, char** argv )
 {
@@ -23,7 +28,7 @@ int main( int argc, char** argv )
     FileStorage fs("dictionary.yml", FileStorage::READ);
     fs["vocabulary"] >> dictionary;
     fs.release();    
-    
+
     //create a nearest neighbor matcher
 
 	Ptr<DescriptorMatcher> matcher(new FlannBasedMatcher);
@@ -40,27 +45,71 @@ int main( int argc, char** argv )
 
     bowDE.setVocabulary(dictionary);
  
-    //open the file to write the resultant descriptor
+ 	vector<Mat> bowKeyFrame;
+	Mat imgCurrent;
+	vector<KeyPoint> keypoints; 
+    Mat bowDescriptor;     
+    Mat test;   
+    //extract BoW (or BoF) descriptor from given image
+    // vector< vector<int> > pointIdxOfClusters;
 
+    for (int i = 0; i < numBOF; i++)
+    {
+    	stringstream imgName;
+		imgName << imgDir << i << imgType;
+		imgCurrent = imread(imgName.str(), CV_LOAD_IMAGE_GRAYSCALE);
+    	detector->detect(imgCurrent, keypoints);
+    	bowDE.compute(imgCurrent, keypoints, bowDescriptor);
+    	// cout << bowDescriptor.size() << endl;
+    	bowKeyFrame.push_back(bowDescriptor);
+    	// cout << bowDescriptor << endl;
+    	// test = bowDescriptor;
+    }
+    // cout << bowKeyFrame[0] << endl;
+
+    //open the file to write the resultant descriptor
     FileStorage fs1("descriptor.yml", FileStorage::WRITE);    
 
-    std::vector<KeyPoint> keypoints;        
+    // vector<KeyPoint> keypoints;        
     //Detect SIFT keypoints (or feature points)
 	Mat cameraFrame, imgGray;
 	cap >> cameraFrame;
 	cvtColor(cameraFrame, imgGray, COLOR_BGR2GRAY);
-
     detector->detect(cameraFrame, keypoints);
     //To store the BoW (or BoF) representation of the image
-
-    Mat bowDescriptor;        
+    // Mat bowDescriptor;        
     //extract BoW (or BoF) descriptor from given image
+    // vector< vector<int> > pointIdxOfClusters;
 
     bowDE.compute(cameraFrame, keypoints, bowDescriptor);
-      
     //write the new BoF descriptor to the file
-
     fs1 << "test" << bowDescriptor;        
-
     fs1.release();
+
+    // vector<DMatch> matches;
+    // matcher -> match(bowDescriptor, matches);
+	int best=0;
+	double minDist = 999999999;
+	for (size_t i=0; i<bowKeyFrame.size(); i++)
+	{
+	     double dist = norm(bowKeyFrame[i], bowDescriptor); //calc L2 distance
+	     if (dist < minDist) // keep the one with smallest distance
+	     {
+	          minDist = dist;
+	          best = i;
+	     }
+	}
+
+	cout << best << endl << minDist << endl;
+
+    // std::vector< std::vector<DMatch> > nn_matches;
+    // matcher.knnMatch(bowDescriptor, nn_matches, 1);
+    // cout << matches.size() << endl << matches[0].imgIdx << endl;
+    // cout << matches[0].trainIdx << endl << matches[1].trainIdx << endl << matches[2].trainIdx << endl << matches[2].trainIdx << endl;
+    // std::cout << nn_matches.size() << std::endl;
+   //  for (int i=0; i<pointIdxOfClusters.size();i++){
+   //  	cout << pointIdxOfClusters[1][i] << endl;
+ 	 // }
+
+
 }
